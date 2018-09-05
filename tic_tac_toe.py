88319071b9e46d667a020a6d18975b9cc32bc98e
Adam Hosman <github@adamsinbox.com>
2018-09-05 14:41:01 +0200

    Little refactor
    Separated input of board_size and to_win into functions.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,23 +1,73 @@
+from sys import exit
+from termcolor import colored
+
+
+def did_player_win(player):
+  stop = to_win-1
+  shapes = {"ud":   {"range_y": (0, board_size - stop),
+                     "range_x": (0, board_size),
+                     "step_y": 1,
+                     "step_x": 0},
+
+            "lr":   {"range_y": (0, board_size),
+                     "range_x": (0, board_size - stop),
+                     "step_y": 0,
+                     "step_x": 1},
+
+            "ullr": {"range_y": (0, board_size - stop),
+                     "range_x": (0, board_size - stop),
+                     "step_y": 1,
+                     "step_x": 1},
+
+            "urll": {"range_y": (2, board_size - stop),
+                     "range_x": (0, board_size - stop),
+                     "step_y": -1,
+                     "step_x": 1}}
+  
+  for shape in shapes.values():
+    for y in range(*shape["range_y"]):
+      for x in range(*shape["range_x"]):
+        if [board[y + shape["step_y"]*i][x + shape["step_x"]*i] for i in range(to_win)] == [MARKS[player]]*to_win:
+          return True
+
 def generate_board():
   return ([[EMPTY]*board_size for i in range(board_size)])
 
-# ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
+def get_board_size(prompt="What size board (from 3-9) would you like to play on? "):
+  """Determines actual playing area without headers, spacing, etc."""
+  return int(input(prompt))
 
-from termcolor import colored
+def get_player_names():
+  return [input("Enter Player 1 name: "), input("Enter Player 2 name: ")]
+
+def get_to_win():
+  return int(input("How many marks in a row to win? "))
+
+def placement(player):
+  coordinates = input(f"{players[player]}, enter your coordinates (e.g. a1, c2): ")
+  # ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
+  if coordinates[0].lower() == 's':
+    save()
+    exit()
+  if coordinates[0].lower() == 'q':
+    exit()
+  # ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑
+  row = int(coordinates[1:])-1
+  column = COLUMNS.index(coordinates[0].upper())
+  if board[row][column] == EMPTY:
+    board[row][column] = MARKS[player]
+  else:
+    print("That's spot is already taken.")
+    placement(player)
 
 # def print_board():
+#   """v0: For testing purposes"""
 #   print(100*'\n')
 #   for row in board:
 #     print(row)
 
-# def print_board():
-#   print(100*'\n')
-#   for row in board:
-#     for char in row:
-#       print(char, end='')
-#     print('')
-
 def print_board():
+  """v1: Minimalistic version without grid, with bold marks"""
   def print_column_headers():
     print('  ', end='')
     for i in range(board_size):
@@ -44,81 +94,36 @@ def print_board():
   print_column_headers() # A
   # print_column_footers() # B
 
-# ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑
-
-    
-def placement(player):
-  coordinates = input(f"{players[player]}, enter your coordinates (e.g. a1, c2): ")
-  row = int(coordinates[1:])-1
-  column = COLUMNS.index(coordinates[0].upper())
-  if board[row][column] == EMPTY:
-    board[row][column] = MARKS[player]
-  else:
-    print("That's spot is already taken.")
-    placement(player)
-
-def did_player_win(player):
-  stop = to_win-1
-  shapes = {
-            "ud":   {"range_y": (0, board_size - stop),
-                     "range_x": (0, board_size),
-                     "step_y": 1,
-                     "step_x": 0},
-
-            "lr":   {"range_y": (0, board_size),
-                     "range_x": (0, board_size - stop),
-                     "step_y": 0,
-                     "step_x": 1},
-
-            "ullr": {"range_y": (0, board_size - stop),
-                     "range_x": (0, board_size - stop),
-                     "step_y": 1,
-                     "step_x": 1},
-
-            "urll": {"range_y": (2, board_size - stop),
-                     "range_x": (0, board_size - stop),
-                     "step_y": -1,
-                     "step_x": 1}
-  }
-  
-  for shape in shapes.values():
-    for y in range(*shape["range_y"]):
-      for x in range(*shape["range_x"]):
-        if [board[y + shape["step_y"]*i][x + shape["step_x"]*i] for i in range(to_win)] == [MARKS[player]]*to_win:
-          return True
-  
-def get_player_names():
-  return [input("Enter Player 1 name: "), input("Enter Player 2 name: ")]
-
 
 # ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓
-
 import pickle
 def save():
     save = players, board, scores, board_size, to_win
     with open("save.pickle", "wb") as file:
       pickle.dump(save, file)
-
 # ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑
 
+
 print(100*'\n')
-COLUMNS = 'ABCDEFGHIJ'
+
+COLUMNS = 'ABCDEFGHI'
 MARKS = 'XO'
 EMPTY = ' '
-board_size = int(input("What size board (from 3-10) would you like to play on? ")) # Actual playing area (1 smaller than matrix)
-to_win = int(input("How many marks in a row to win? "))
+board_size = get_board_size()
+to_win = get_to_win()
 players = get_player_names()
 
 board = []
 scores = [0, 0]
 wants_to_play = True
 
+
 while wants_to_play:
   board = generate_board()
   print_board()
   winner = None
   while not winner:
-    for player in range(2):
+    for player in range(2):      
       placement(player)
       print_board()
       if did_player_win(player):
