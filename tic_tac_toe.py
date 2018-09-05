36a13a1ff03409019484d40e66f1eac64135573e
Adam Hosman <github@adamsinbox.com>
2018-09-05 10:36:04 +0200

    Separated print logic (0-index now)
    Separated creation of header rows and columns out of generate_board(), in order to enable creation of multiple designs in print_board and make the board 0-index based. (And, generally, to follow the best practice of 'separation of concerns'.)

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,15 +1,6 @@
-# def generate_board():
-#   """Readable version"""
-#   board = [[char for char in COLUMNS[:board_size + 1]]]
-#   for i in range(board_size):
-#     row = [str(i+1)] + [' ']*board_size
-#     board.append(row)
-#   return board
-
 def generate_board():
   """Short version"""
-  return ([[char for char in COLUMNS[:board_size + 1]]]
-         + [[str(i+1)] + [' ']*board_size for i in range(board_size)])
+  return ([[EMPTY]*board_size for i in range(board_size)])
 
 def print_board():
   print(100*'\n')
@@ -25,9 +16,9 @@ def print_board():
     
 def placement(player):
   coordinates = input(f"{players[player]}, enter your coordinates (e.g. a1, c2): ")
-  row = int(coordinates[1:])
+  row = int(coordinates[1:])-1
   column = COLUMNS.index(coordinates[0].upper())
-  if board[row][column] == " ":
+  if board[row][column] == EMPTY:
     board[row][column] = MARKS[player]
   else:
     print("That's spot is already taken.")
@@ -36,30 +27,30 @@ def placement(player):
 def did_player_win(player):
   stop = to_win-1
   shapes = {
-            "ud":   {"range_y": (1, board_size+1 - stop),
-                     "range_x": board_size+1,
+            "ud":   {"range_y": (0, board_size - stop),
+                     "range_x": (0, board_size),
                      "step_y": 1,
                      "step_x": 0},
 
-            "lr":   {"range_y": (1, board_size+1),
-                     "range_x": board_size+1 - stop,
+            "lr":   {"range_y": (0, board_size),
+                     "range_x": (0, board_size - stop),
                      "step_y": 0,
                      "step_x": 1},
 
-            "ullr": {"range_y": (1, board_size+1 - stop),
-                     "range_x": board_size+1 - stop,
+            "ullr": {"range_y": (0, board_size - stop),
+                     "range_x": (0, board_size - stop),
                      "step_y": 1,
                      "step_x": 1},
 
-            "urll": {"range_y": (3, board_size+1 - stop),
-                     "range_x": board_size+1 - stop,
+            "urll": {"range_y": (2, board_size - stop),
+                     "range_x": (0, board_size - stop),
                      "step_y": -1,
                      "step_x": 1}
   }
   
   for shape in shapes.values():
     for y in range(*shape["range_y"]):
-      for x in range(shape["range_x"]):
+      for x in range(*shape["range_x"]):
         if [board[y + shape["step_y"]*i][x + shape["step_x"]*i] for i in range(to_win)] == [MARKS[player]]*to_win:
           return True
   
@@ -68,10 +59,9 @@ def get_player_names():
 
 
 print(100*'\n')
-COLUMNS = ' ABCDEFGHIJ'
-# COLUMNS = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-# ROWS = [' ', '1  ', '2  ', '3  ', '4  ', '5  ', '6  ', '7  ', '8  ', '9  ', '10 ']
+COLUMNS = 'ABCDEFGHIJ'
 MARKS = 'XO'
+EMPTY = ' '
 board_size = int(input("What size board (from 3-10) would you like to play on? ")) # Actual playing area (1 smaller than matrix)
 to_win = int(input("How many marks in a row to win? "))
 players = get_player_names()
