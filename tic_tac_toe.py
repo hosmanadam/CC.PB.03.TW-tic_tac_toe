66a400eb845082be815ab44fb42d7138d1f683b0
Adam Hosman <github@adamsinbox.com>
2018-09-17 19:07:53 +0200

    Created `class Game`
    Replaces functionality of `data.variables` module. Instantiated as `g` and adjusted references in `main()`. In full working order.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,67 +5,81 @@ from time import sleep
 import pickle
 
 
+class Game:
+  def __init__(self):
+    # Fixed at new game start 
+    self.board_size = None
+    self.winning_size = None
+    self.players = []
+    # Changes after every win
+    self.scores = []
+    self.winning_row = [] # (x, y) coordinates (to be indexed as board[y][x])
+    # Changes with every step
+    self.board = []
+    self.steps = []       # (x, y) coordinates (to be indexed as board[y][x])
+
+
 if __name__ == '__main__':
   def main():
-    import data.variables as v
     import data.constants as c
     import functions.all as f
+    g = Game()
     try:
       system('clear')
       try:
-        v.board_size, v.winning_size, v.players, v.scores, v.board, v.steps, current_player = f.game_load()
+        g.board_size, g.winning_size, g.players, g.scores, g.board, g.steps, current_player = f.game_load()
         print(c.WELCOME_BACK[0]); sleep(c.WAIT)
         print(c.WELCOME_BACK[1]); sleep(c.WAIT)
         from_load = True                                    # HACK 1
       except FileNotFoundError:
         print(c.WELCOME); sleep(c.WAIT)
-        v.board_size, v.winning_size, v.players, v.scores, current_player = f.game_new()
+        g.board_size, g.winning_size, g.players, g.scores, current_player = f.game_new()
         print("\nLet's begin..."); sleep(c.WAIT)
         from_load = False                                   # HACK 1
       wants_to_play = True
       while wants_to_play:
         if not from_load:                                   # HACK 1 - resets round variables if game is new, not loaded
-          v.board = f.generate_board(c.EMPTY, v.board_size) # HACK 1
-          v.steps = [[], []]                                # HACK 1
+          g.board = f.generate_board(c.EMPTY, g.board_size) # HACK 1
+          g.steps = [[], []]                                # HACK 1
         from_load = False                                   # HACK 1
         winner = None
         while winner == None:
           for player in range(2):
             system('clear')
-            if player == 0 and len(v.steps[0]) > len(v.steps[1]):
+            if player == 0 and len(g.steps[0]) > len(g.steps[1]):
               continue # Makes loaded game start with next player
-            f.print_scores(v.players, v.scores, c.COLORS); print('')
+            f.print_scores(g.players, g.scores, c.COLORS); print('')
             print(c.INSTRUCTIONS)
             last_player = [x for x in (0, 1) if x != player][0]
-            f.print_board(last_player, winner, v.board_size, c.COLUMNS, 
-                          v.winning_row, v.board, v.steps)
-            print(colored(f"\n{v.players[player]}", c.COLORS[player], attrs=['bold']) +
+            f.print_board(last_player, winner, g.board_size, c.COLUMNS, 
+                          g.winning_row, g.board, g.steps)
+            print(colored(f"\n{g.players[player]}", c.COLORS[player], attrs=['bold']) +
                            ", make your move: ", end='')
-            f.prompt_action(player, c.COLUMNS, c.EMPTY, c.MARKS, v.board, v.steps,  # for place_mark()
+            f.prompt_action(player, c.COLUMNS, c.EMPTY, c.MARKS, g.board, g.steps,  # for place_mark()
                             c.GOODBYE, c.WAIT,                                       # for quit()
-                            v.board_size, v.winning_size, v.players, v.scores,       # for game_save()
-                            v.board, v.steps, current_player)                        # for game_save()
-            v.winning_row = f.did_player_win(player, v.winning_size, v.board_size, v.board, c.MARKS)
-            if v.winning_row:
+                            g.board_size, g.winning_size, g.players, g.scores,       # for game_save()
+                            g.board, g.steps, current_player)                        # for game_save()
+            g.winning_row = f.did_player_win(player, g.winning_size, g.board_size, g.board, c.MARKS)
+            if g.winning_row:
               winner = player
               system('clear')
               print('\n'*5)
-              f.print_board(last_player, winner, v.board_size, c.COLUMNS, 
-                            v.winning_row, v.board, v.steps)
-              print(colored(f"\n{v.players[player]} wins in {len(v.steps[player])} "
+              f.print_board(last_player, winner, g.board_size, c.COLUMNS, 
+                            g.winning_row, g.board, g.steps)
+              print(colored(f"\n{g.players[player]} wins in {len(g.steps[player])} "
                              "steps!", c.COLORS[player], attrs=['bold'])); sleep(c.WAIT)
-              v.scores[player] += 1
-              f.print_scores(v.players, v.scores, c.COLORS); sleep(c.WAIT)
+              g.scores[player] += 1
+              f.print_scores(g.players, g.scores, c.COLORS); sleep(c.WAIT)
               wants_to_play = f.wants_rematch()
               break
-            if f.is_it_a_tie(v.steps, v.board_size):
+            if f.is_it_a_tie(g.steps, g.board_size):
               winner = 'tie'                                                  # HACK 2 - duplicate of winning scenario
               system('clear')                                                 # HACK 2   w/ minor modifications
               print('\n'*5)                                                   # HACK 2
-              f.print_board(last_player, winner, v.board_size, c.COLUMNS,     # HACK 2
-                            v.winning_row, v.board, v.steps)                  # HACK 2
+              f.print_board(last_player, winner, g.board_size, c.COLUMNS,     # HACK 2
+                            g.winning_row, g.board, g.steps)                  # HACK 2
               print(colored("\nIt's a tie!", attrs=['bold'])); sleep(c.WAIT)  # HACK 2
-              f.print_scores(v.players, v.scores, c.COLORS); sleep(c.WAIT)    # HACK 2
+              f.print_scores(g.players, g.scores, c.COLORS); sleep(c.WAIT)    # HACK 2
               wants_to_play = f.wants_rematch()                               # HACK 2
               break                                                           # HACK 2
       f.quit(c.GOODBYE, c.WAIT)
