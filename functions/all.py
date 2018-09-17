09e73d7919f5dcf1e93606d261e75ed19c680251
Adam Hosman <github@adamsinbox.com>
2018-09-17 21:39:34 +0200

    Minor
    Renamed Game instance from `g` to `game`

--- functions/all.py
+++ functions/all.py
@@ -7,36 +7,36 @@ import pickle
 from data.constants import *
 
 
-def did_player_win(player, g):
+def did_player_win(player, game):
   """TODO: write documentation"""
-  stop = g.winning_size-1
-  shapes = {"ud":   {"range_y": (0, g.board_size - stop),
-                     "range_x": (0, g.board_size),
+  stop = game.winning_size-1
+  shapes = {"ud":   {"range_y": (0, game.board_size - stop),
+                     "range_x": (0, game.board_size),
                       "step_y": 1,
                       "step_x": 0},
 
-            "lr":   {"range_y": (0, g.board_size),
-                     "range_x": (0, g.board_size - stop),
+            "lr":   {"range_y": (0, game.board_size),
+                     "range_x": (0, game.board_size - stop),
                       "step_y": 0,
                       "step_x": 1},
 
-            "ullr": {"range_y": (0, g.board_size - stop),
-                     "range_x": (0, g.board_size - stop),
+            "ullr": {"range_y": (0, game.board_size - stop),
+                     "range_x": (0, game.board_size - stop),
                       "step_y": 1,
                       "step_x": 1},
 
-            "urll": {"range_y": (0, g.board_size - stop),
-                     "range_x": (2, g.board_size),
+            "urll": {"range_y": (0, game.board_size - stop),
+                     "range_x": (2, game.board_size),
                       "step_y": 1,
                       "step_x": -1}}
 
   for shape in shapes.values():
     for y in range(*shape["range_y"]):
       for x in range(*shape["range_x"]):
-        if ([g.board[y + shape["step_y"]*i][x + shape["step_x"]*i]           # TODO - remove duplication
-            for i in range(g.winning_size)] == [MARKS[player]]*g.winning_size):
-          winning_row = [((x + shape["step_x"]*i), (y + shape["step_y"]*i))  # TODO - remove duplication
-                         for i in range(g.winning_size)]
+        if ([game.board[y + shape["step_y"]*i][x + shape["step_x"]*i]           # TODO - remove duplication
+            for i in range(game.winning_size)] == [MARKS[player]]*game.winning_size):
+          winning_row = [((x + shape["step_x"]*i), (y + shape["step_y"]*i))     # TODO - remove duplication
+                         for i in range(game.winning_size)]
           return winning_row
 
 def game_load():
@@ -46,10 +46,10 @@ def game_load():
   remove("saved.pickle")
   return payload
 
-def game_save(g):
+def game_save(game):
   """TODO: write documentation"""
   with open("saved.pickle", "wb") as file:
-    pickle.dump(g, file)
+    pickle.dump(game, file)
   print("Game has been saved.")
 
 def generate_board(board_size):
@@ -106,37 +106,37 @@ def is_it_a_tie(steps, board_size):
   if len(steps[0]) + len(steps[1]) == board_size**2:
     return True
 
-def place_mark(coordinates, player, g):
+def place_mark(coordinates, player, game):
   """TODO: write documentation"""
   row = int(coordinates[1:])-1
   if row < 0:
     raise IndexError
   column = COLUMNS.index(coordinates[0].upper())
-  if g.board[row][column] == EMPTY:
-    g.board[row][column] = MARKS[player]
-    g.steps[player].append((column, row))
+  if game.board[row][column] == EMPTY:
+    game.board[row][column] = MARKS[player]
+    game.steps[player].append((column, row))
   else:
-    prompt_action(player, g,
+    prompt_action(player, game,
                   prompt="That spot is already taken. Try again: ")
 
-def prompt_action(player, g, prompt=''):
+def prompt_action(player, game, prompt=''):
   """TODO: write documentation"""
   try:
     action = input(prompt)
     if action.lower() == 's':
-      game_save(g); sleep(WAIT/2)
+      game_save(game); sleep(WAIT/2)
       quit()
     if action.lower() == 'q':
       quit()
-    place_mark(action, player, g)
+    place_mark(action, player, game)
   except IndexError:
-    prompt_action(player, g,
+    prompt_action(player, game,
                   prompt="Coordinates out of range. Try again: ")
   except ValueError:
-    prompt_action(player, g,
+    prompt_action(player, game,
                   prompt="Incorrectly formatted coordinates. Try again: ")
 
-def print_board(last_player, g):
+def print_board(last_player, game):
   """TODO: write documentation
   Minimalistic version without grid, with bold marks"""
   def print_column_headers(board_size):
@@ -145,24 +145,24 @@ def print_board(last_player, g):
       print(COLUMNS[i] + ' ', end='')
     print(' ')
 
-  def print_rows(last_player, g):
-    for row in range(g.board_size):
+  def print_rows(last_player, game):
+    for row in range(game.board_size):
       print(str(row+1) + ' ', end='')
-      for place in range(g.board_size):
-        if g.winner in (0, 1) and (place, row) in g.winning_row: # mark as winning row
-          print(colored(g.board[row][place], attrs=['bold']) +
+      for place in range(game.board_size):
+        if game.winner in (0, 1) and (place, row) in game.winning_row: # mark as winning row
+          print(colored(game.board[row][place], attrs=['bold']) +
                 colored('←', 'blue', attrs=['bold']), end='')
-        elif (g.winner == None and g.steps[last_player]          # mark as last step
-              and (place, row) == g.steps[last_player][-1]):
-          print(colored(g.board[row][place], attrs=['bold']) + 
+        elif (game.winner == None and game.steps[last_player]          # mark as last step
+              and (place, row) == game.steps[last_player][-1]):
+          print(colored(game.board[row][place], attrs=['bold']) + 
                 colored('←', 'blue', attrs=['bold']), end='')
-        else:                                                    # print w/o marking
-          print(colored(g.board[row][place], attrs=['bold']) + ' ', end='')
+        else:                                                          # print w/o marking
+          print(colored(game.board[row][place], attrs=['bold']) + ' ', end='')
       print(str(row+1))
 
-  print_column_headers(g.board_size)
-  print_rows(last_player, g)
-  print_column_headers(g.board_size)
+  print_column_headers(game.board_size)
+  print_rows(last_player, game)
+  print_column_headers(game.board_size)
 
 def print_scores(players, scores):
   """TODO: write documentation"""
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -29,53 +29,53 @@ if __name__ == '__main__':
     try:
       system('clear')
       try:
-        g = f.game_load()
+        game = f.game_load()
         print(WELCOME_BACK[0]); sleep(WAIT)
         print(WELCOME_BACK[1]); sleep(WAIT)
-        from_load = True                           # HACK 1
+        from_load = True                                # HACK 1
       except FileNotFoundError:
         print(WELCOME); sleep(WAIT)
-        g = Game()
+        game = Game()
         print("\nLet's begin..."); sleep(WAIT)
-        from_load = False                          # HACK 1
+        from_load = False                                # HACK 1
       wants_to_play = True
       while wants_to_play:
-        if not from_load:                          # HACK 1 - resets round variables if game is new, not loaded
-          g.board = f.generate_board(g.board_size) # HACK 1 - TODO: implement more elegant reset() method
-          g.steps = [[], []]                       # HACK 1
-        from_load = False                          # HACK 1
-        g.winner = None
-        while g.winner == None:
+        if not from_load:                                # HACK 1 - resets round variables if game is new, not loaded
+          game.board = f.generate_board(game.board_size) # HACK 1 - TODO: implement more elegant reset() method
+          game.steps = [[], []]                          # HACK 1
+        from_load = False                                # HACK 1
+        game.winner = None
+        while game.winner == None:
           for player in range(2):
             system('clear')
-            if player == 0 and len(g.steps[0]) > len(g.steps[1]):
+            if player == 0 and len(game.steps[0]) > len(game.steps[1]):
               continue # Makes loaded game start with next player
-            f.print_scores(g.players, g.scores); print('')
+            f.print_scores(game.players, game.scores); print('')
             print(INSTRUCTIONS)
             last_player = [x for x in (0, 1) if x != player][0]
-            f.print_board(last_player, g)
-            print(colored(f"\n{g.players[player]}", COLORS[player], attrs=['bold']) +
+            f.print_board(last_player, game)
+            print(colored(f"\n{game.players[player]}", COLORS[player], attrs=['bold']) +
                            ", make your move: ", end='')
-            f.prompt_action(player, g)
-            g.winning_row = f.did_player_win(player, g)
-            if g.winning_row:
-              g.winner = player
+            f.prompt_action(player, game)
+            game.winning_row = f.did_player_win(player, game)
+            if game.winning_row:
+              game.winner = player
               system('clear')
               print('\n'*5)
-              f.print_board(last_player, g)
-              print(colored(f"\n{g.players[player]} wins in {len(g.steps[player])} "
+              f.print_board(last_player, game)
+              print(colored(f"\n{game.players[player]} wins in {len(game.steps[player])} "
                              "steps!", COLORS[player], attrs=['bold'])); sleep(WAIT)
-              g.scores[player] += 1
-              f.print_scores(g.players, g.scores); sleep(WAIT)
+              game.scores[player] += 1
+              f.print_scores(game.players, game.scores); sleep(WAIT)
               wants_to_play = f.wants_rematch()
               break
-            if f.is_it_a_tie(g.steps, g.board_size):
-              g.winner = 'tie'                                                # HACK 2 - duplicate of winning scenario
+            if f.is_it_a_tie(game.steps, game.board_size):
+              game.winner = 'tie'                                             # HACK 2 - duplicate of winning scenario
               system('clear')                                                 # HACK 2   w/ minor modifications
               print('\n'*5)                                                   # HACK 2
-              f.print_board(last_player, g)                                   # HACK 2
+              f.print_board(last_player, game)                                # HACK 2
               print(colored("\nIt's a tie!", attrs=['bold'])); sleep(WAIT)    # HACK 2
-              f.print_scores(g.players, g.scores); sleep(WAIT)                # HACK 2
+              f.print_scores(game.players, game.scores); sleep(WAIT)          # HACK 2
               wants_to_play = f.wants_rematch()                               # HACK 2
               break                                                           # HACK 2
       f.quit()
